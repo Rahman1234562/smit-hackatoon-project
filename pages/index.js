e9d79799b3cb00535bab4aae8939ea7bb3a8bc6a
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-08-19 07:57:43 -0700

    updated file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import NavOut from '../Components/NavOut'
 import { HomePage } from '../Components/homepage'
+import Navbar from '../Components/Navbar'
 
 
 
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Blog App</title>
+        <title>smit hackatoon project</title>
         <meta name="description" content="Create Your own blog" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
